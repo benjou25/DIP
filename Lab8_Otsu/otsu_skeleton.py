151738456ff7b1e136b49602a994d8132e1126d2
Benjamin <bechtben@students.zhaw.ch>
2023-11-08 14:44:09 +0100

    otsu done

--- Lab8_Otsu/otsu_skeleton.py
+++ Lab8_Otsu/otsu_skeleton.py
@@ -23,16 +23,56 @@ def basic_thresholding(image):
     return bin_img, t
 
 
+import numpy as np
+
 def my_otsu(image):
-
-    # dummy variables, replace with your own code:
-    threshold = 128
-    between_class_variance = 0
+    # Calculate the histogram of the input image
+    hist, bins = np.histogram(image, bins=256, range=(0, 256))
+    
+    # Compute total number of pixels
+    total_pixels = np.sum(hist)
+    # total mean value
+    m_G = np.dot(np.arange(256), hist) / total_pixels
+    # array for between class variances
+    between_class_array = np.empty(256)
+    
+    # initializations
+    between_class_variance_max = 0
+    threshold = 0
     separability = 0
-    # until here
 
+    for k in range(1, 256):
+        class_a = hist[:k]
+        class_b = hist[k:]
+        
+        sum_a = np.sum(class_a)
+        sum_b = np.sum(class_b)
+        
+        # Compute the probabilities of each class
+        prob_a = sum_a / total_pixels
+        prob_b = sum_b / total_pixels
+        
+        # Calculate the mean of each class
+        m_a = np.dot(np.arange(k), class_a) / sum_a if sum_a > 0 else 0
+        m_b = np.dot(np.arange(k, 256), class_b) / sum_b if sum_b > 0 else 0
+        
+        # Calculate between-class variance
+        between_class_variance = prob_a * (m_a - m_G) ** 2 + prob_b * (m_b - m_G) ** 2
+        G_variance = (k - m_G) ** 2
+        between_class_array[k] = between_class_variance
+
+        if between_class_variance > between_class_variance_max:
+            between_class_variance_max = between_class_variance
+            threshold = k
+            separability = between_class_variance / G_variance
+
+    # Apply the threshold to the image
     binary_image = image >= threshold
-    return binary_image, between_class_variance, threshold, separability
+    
+
+    return binary_image, between_class_array, threshold, separability
+
+
 
 
 def main():
@@ -62,6 +102,7 @@ def main():
     #print(separability)
 
     plt.plot(between_class_variance*100)
+    plt.title("between class variance")
     plt.show()
 
     plt.imshow(binary_image.astype(int), cmap="gray")
