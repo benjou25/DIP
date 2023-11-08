0d39e5f94eeca7237b8fc6e3b9f0a49aef49e8fe
Benjamin <bechtben@students.zhaw.ch>
2023-11-08 12:18:41 +0100

    basic läuft

--- Lab8_Otsu/otsu_skeleton.py
+++ Lab8_Otsu/otsu_skeleton.py
@@ -5,14 +5,23 @@ import cv2 as cv
 from PIL import Image
 
 def basic_thresholding(image):
-    t = np.mean(image.flatten()).astype(int)
+    imnew = image.flatten()
+    t = np.mean(image).astype(int)
+    while True:
+        seg_a = imnew[imnew > t]
+        seg_b = imnew[imnew <= t]
+        t_a = np.mean(seg_a)
+        t_b = np.mean(seg_b)
+        new_t = np.round((t_a + t_b) / 2)
 
-    # Calculate the basic threshold
+        if np.abs(t - new_t) < 0.1:
+            break
 
-    # <-- your input
+        t = new_t
 
     bin_img = image > t
     return bin_img, t
+
 
 def my_otsu(image):
 
@@ -37,7 +46,13 @@ def main():
     binary_image, t = basic_thresholding(image)
     print("Basic Thresholding. Output Threshold: "+str(t))
 
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
     plt.imshow(binary_image.astype(int), cmap="gray")
+    plt.title("basic thresholding")
+    plt.subplot(1, 2, 2)
+    plt.hist(image.ravel(), bins=256)
+    plt.axvline(t, color='r', linestyle='dashed', linewidth=2)
     plt.show()
 
     binary_image, between_class_variance, threshold, separability = my_otsu(image)
