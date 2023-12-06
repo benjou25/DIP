aafb82b1bdfb55c8b7e1053f534cbb1e00f5685d
Benjamin <bechtben@students.zhaw.ch>
2023-12-06 12:33:36 +0100

    done

---
+++ Lab12_Homography/Homography_Skeleton.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+FIGURE_SIZE = (12, 9)
+imgFolder = ''
+imgName = 'chessboard_perspective.jpg'
+
+img_persp = plt.imread(imgFolder + imgName)
+
+# Coordinates are defined as [vertical, horizontal]
+if imgName == "chessboard_perspective.jpg":
+    Ps = np.array([[45, 385], [420, 55], [665, 723], [187, 928]])   # TODO: Additional Points go here
+    points_u = np.array([[0, 0], [700, 0], [700, 700], [0, 700]])   # TODO: Coordinates of undistorted Points go here
+
+def plot_points_on_image(img: np.ndarray, points: np.ndarray):
+    plt.figure()
+    plt.imshow(img)
+    for point in points:
+        plt.plot(point[1], point[0], 'o')    
+    plt.title("Image with Points at Specified Coordinates")    
+    plt.xlabel("Coordinate 1")
+    plt.ylabel("Coordinate 0")
+    plt.show()
+
+plot_points_on_image(img_persp, Ps)
+
+# Create the matrix A
+A = np.zeros((8, 8))
+y = np.zeros((8,))
+
+for i in range(len(Ps)):
+    y_d, x_d = Ps[i]
+    y_u, x_u = points_u[i]
+    y[2 * i] = x_d
+    y[2 * i + 1] = y_d
+    A[2 * i, :] = [x_u, y_u, 1, 0, 0, 0, -x_u*x_d, -y_u*x_d]
+    A[2 * i + 1, :] = [0, 0, 0, x_u, y_u, 1, -y_d*x_u, -y_d*y_u]
+
+# Solve the system of linear equations
+solution = np.linalg.solve(A,y)
+H = np.array([[solution[0], solution[1], solution[2]],
+              [solution[3], solution[4], solution[5]],
+              [solution[6], solution[7], 1]])
+print(solution)
+H = np.linalg.inv(H)
+# Undistort the image
+undistorted_img = cv2.warpPerspective(img_persp, H, (img_persp.shape[0], img_persp.shape[0]))
+
+# Display the undistorted image using cv2.imshow
+cv2.imshow("Undistorted Image", undistorted_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- yeeehaw/grum.py
+++ yeeehaw/grum.py
@@ -1,54 +1,57 @@
 import cv2
 import numpy as np
 
-def process_frame(frame):
-    # Convert the frame to HSV color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def process_frame(frame, lower_bound, upper_bound):
+    # Create a mask for colors within the specified range
+    mask = cv2.inRange(frame, lower_bound, upper_bound)
 
-    # Define a color range for detecting skin color (you may need to adjust these values)
-    lower_red = np.array([0, 100, 100], dtype=np.uint8)
-    upper_red = np.array([10, 255, 255], dtype=np.uint8)
-
-    # Create a binary mask using the inRange function
-    mask = cv2.inRange(hsv, lower_red, upper_red)
-
-    # Perform morphological operations to remove noise
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
-
-    # Apply the mask to the original frame
+    # Bitwise AND to keep colors within the specified range
     result = cv2.bitwise_and(frame, frame, mask=mask)
 
-    return result
+    # Invert the mask to get the region outside the specified range
+    inverse_mask = cv2.bitwise_not(mask)
+
+    # Create a color gradient for the background
+    gradient = np.zeros_like(frame)
+    gradient[:, :, 0] = np.linspace(0, 255, frame.shape[0])[:, np.newaxis]
+    gradient[:, :, 1] = np.linspace(0, 255, frame.shape[0])[:, np.newaxis]
+    gradient[:, :, 2] = np.linspace(0, 255, frame.shape[0])[:, np.newaxis]
+
+    # Bitwise AND to keep the gradient in the region outside the specified range
+    result_bg = cv2.bitwise_and(gradient, gradient, mask=inverse_mask)
+
+    # Combine the foreground and background
+    result = cv2.add(result, result_bg)
+
+    # Convert the result to grayscale
+    result_gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    return result_gray
 
 def main():
-    # Open a connection to the webcam (usually 0 for built-in webcams)
     cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         return
 
+    lower_bound = np.array([200, 200, 200], dtype=np.uint8)
+    upper_bound = np.array([255, 255, 255], dtype=np.uint8)
+
     while True:
-        # Read a frame from the webcam
         ret, frame = cap.read()
 
         if not ret:
             print("Error: Could not read frame.")
             break
 
-        # Process the frame to display only skin
-        processed_frame = process_frame(frame)
+        processed_frame = process_frame(frame, lower_bound, upper_bound)
 
-        # Display the original and processed frames
-        cv2.imshow('Original Frame', frame)
-        cv2.imshow('Processed Frame', processed_frame)
+        cv2.imshow('Processed Frame (Color Range)', processed_frame)
 
-        # Break the loop when 'Esc' key is pressed
-        if cv2.waitKey(1) & 0xFF == 27:  # 27 is the ASCII code for 'Esc' key
+        if cv2.waitKey(1) & 0xFF == 27:
             break
 
-    # Release the webcam and close all windows
     cap.release()
     cv2.destroyAllWindows()
 
